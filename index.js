997f7c74bd9b812a0e12c90b2149178695eaebc9
Jose <gldmz0318@icloud.com>
2023-01-23 19:40:50 -0800

    Addin places controller

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').configure()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+
+// part six: make a controller
